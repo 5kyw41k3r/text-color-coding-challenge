29f768f1dcd6c0ba6e341dd9dd755a5f5f077555
5kyw41k3r <67189930+5kyw41k3r@users.noreply.github.com>
2020-12-03 12:12:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+userString = document.getElementById("string").value;
+
+// function countChar(val) {
+//   var len = document.getElementById("string").value.length;
+//   document.getElementById("counter").innerHTML = len;
+// }
+
+function charcountupdate() {
+  userString = document.getElementById("string").value;
+  var lng = userString.length;
+  document.getElementById("counter").innerHTML = lng;
+}
+
+// Function to change caret color
+function colorChanger() {
+  var len = document.getElementById("string").value.length;
+  if (len < 50) {
+    document.getElementById("string").classList.remove("second_100");
+    document.getElementById("string").classList.remove("last_140");
+    document.getElementById("string").classList.add("first_50");
+  } else if (len > 50 && len < 100) {
+    document.getElementById("string").classList.remove("first_50");
+    document.getElementById("string").classList.remove("last_140");
+    document.getElementById("string").classList.add("second_100");
+  } else if (len > 100) {
+    document.getElementById("string").classList.remove("first_50");
+    document.getElementById("string").classList.remove("second_100");
+    document.getElementById("string").classList.add("last_140");
+  }
+}
